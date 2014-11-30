d6cc0a1630b31676789096adf83ca5c279fcbe3c
martimatix <zenitram.oiram@gmail.com>
2014-11-30 20:51:34 +0800

    add dice.rb

---
+++ dice.rb
@@ -0,0 +1,38 @@
+# Class for the 5 dice in the game
+
+class Dice
+
+	def initialize()
+		@five_dice = 5.times.map{ 1 + rand(5)}
+	end
+
+	def roll_all()
+		initialize()
+	end
+
+	# Rolls a single dice
+	def roll(i)
+		if i >= 0 && i<=5
+			@five_dice[i-1] = 1 + rand(5)
+		end
+	end
+
+
+	def display()
+		@five_dice.each do |i|
+			puts i
+		end
+		puts "\n"
+	end
+
+end
+
+# testing
+d = Dice.new()
+
+(0..5).each do
+	d.roll_all
+	d.display()
+end
+
+
