d13d14add714620601353d548043d1a0c565d13c
Zachary Perlmutter <zrp200@gmail.com>
2014-12-07 15:59:10 -0800

    Update dice.rb
    Moved FiveDice to a new file

--- lib/dice.rb
+++ lib/dice.rb
@@ -5,27 +5,4 @@ class Dice # Class for a die in the game
 	def roll; @num = Faces.sample; end # Rolls the dice
 	alias to_s num
 end
-class FiveDice # Class for working with the 5 dice at the same time
-	attr_reader :dice # The dice in the game
-	def initialize
-		@dice = Array.new
-		5.times {@dice.push Dice.new}
-	end
-	def roll(i); @dice[i].roll; end # Rolls the die at the index i
-	def roll_all # Rolls all the dice
-		for die in @dice; die.roll; end
-	end
-	def display # Display instance variable dice
-		@dice.each {|i| puts i}
-		puts "\n"
-	end
-end
-# testing
-d = FiveDice.new
-
-d.roll_all
-d.display
-d.sort
-d.display
-
 
