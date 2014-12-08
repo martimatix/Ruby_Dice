58b23a2ef1d97fada53418a9a6555819314c8385
Zachary Perlmutter <zrp200@gmail.com>
2014-12-07 17:32:31 -0800

    Fixed tests
    Signed-off-by: Zachary Perlmutter <zrp200@gmail.com>

--- spec/lib/fivedice_spec.rb
+++ spec/lib/fivedice_spec.rb
@@ -1,9 +1,13 @@
 require "spec_helper"
 require "fivedice"
 RSpec.describe FiveDice do
-  describe "#new" do
-    subject {FiveDice.new}
-    its(:sample) {is_expected.to be_an_instance_of Dice}
-    it {is_expected.to be_kind_of Dice}
-  end
+ 	describe "#new" do
+  		describe "dice" do
+    			subject {FiveDice.new.dice}
+    			its(:sample) {is_expected.to be_an_instance_of Dice}
+		end
+	subject {FiveDice.new}
+    	it {is_expected.to be_kind_of Dice}
+	it {is_expected.to be_an_instance_of FiveDice}
+ 	end
 end
