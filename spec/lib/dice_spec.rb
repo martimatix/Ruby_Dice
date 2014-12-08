63f68a01fb715c1df64e0e4e259b91989a19436d
Zachary Perlmutter <zrp200@gmail.com>
2014-12-07 19:46:16 -0800

    Added tests for Dice::Faces

--- spec/lib/dice_spec.rb
+++ spec/lib/dice_spec.rb
@@ -1,9 +1,11 @@
 require "spec_helper"
 require "dice"
-describe Dice do
+RSpec.describe Dice do
 	describe "#new" do
 		subject {Dice.new}
 		it {is_expected.to respond_to(:roll)}
 	end
 end
-	
+RSpec.describe Dice::Faces do
+	it {is_expected.to eq (1..6).to_a}
+end
