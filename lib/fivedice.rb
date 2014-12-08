076e2843da63e5f8f62a87d292325979978cb417
Zachary Perlmutter <zrp200@gmail.com>
2014-12-07 19:50:07 -0800

    Update fivedice.rb

--- lib/fivedice.rb
+++ lib/fivedice.rb
@@ -3,7 +3,7 @@ class FiveDice < Dice # Class for working with the 5 dice at the same time
 	attr_reader :dice # The dice in the game
 	def initialize
 		@dice = Array.new
-		5.times {@dice.push Dice.new}
+		5.times {@dice.push super}
 	end
 	def roll(i); @dice[i].roll; end # Rolls the die at the index i
 	def roll_all # Rolls all the dice
