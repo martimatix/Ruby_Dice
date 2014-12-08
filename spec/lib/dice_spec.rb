26bfbde7df9f9daf3be05393db4bf5c89a002b43
Zachary Perlmutter <zrp200@gmail.com>
2014-12-07 16:09:55 -0800

    Update dice_spec.rb

--- spec/lib/dice_spec.rb
+++ spec/lib/dice_spec.rb
@@ -1,9 +1,9 @@
-require "rspec"
-require "dice.rb"
+require "spec_helper"
+require "dice"
 describe Dice do
 	describe "#new" do
 		subject {Dice.new}
-		it {is_expected.to respond_to(:display, :sort, :roll, :roll_all)}
+		it {is_expected.to respond_to(:roll)}
 	end
 end
 	
