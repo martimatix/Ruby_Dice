81ba01ef7039f2fbd0bd08cec481c0729ba3c217
Zachary Perlmutter <zrp200@gmail.com>
2014-12-08 19:11:43 -0800

    Update dice.rb

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,15 +1,15 @@
 
 class Dice  # Class for working with the 5 dice at the same time
 	attr_reader :dice # The dice in the game
-	def initialize; @dice = Array.new(5) {dice}; end
-	def roll(i); @dice[i] = dice; end # Rolls the die at the index i
+	def initialize; @dice = Array.new(5) {new_dice}; end
+	def roll(i); @dice[i] = new_dice; end # Rolls the die at the index i
 	def roll_all # Rolls all the dice
-		for die in @dice; die == dice; end
+		for die in @dice; die == new_dice; end
 	end
 	def display # Display instance variable dice
 		@dice.each {|i| puts i}
 		puts "\n"
 	end
 	private 
-	def dice; return (1..6).to_a.sample; end # Returns a random number between 1 and 6, inclusive
+	def new_dice; return (1..6).to_a.sample; end # Returns a random number between 1 and 6, inclusive
 end
