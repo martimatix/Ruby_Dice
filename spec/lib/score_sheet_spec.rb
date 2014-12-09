6c39b1a3ce692816e47bc59b9b60eae639be3380
Zachary Perlmutter <zrp200@gmail.com>
2014-12-08 19:14:51 -0800

    Added dice test

--- spec/lib/score_sheet_spec.rb
+++ spec/lib/score_sheet_spec.rb
@@ -3,6 +3,6 @@ require "score_sheet"
 describe ScoreSheet do
 	describe "#new" do
 		subject {ScoreSheet.new}
-		it {is_expected.to respond_to(:chance, :large_straight, :small_straight, :full_house, :yahtzee, :four_of_a_kind, :three_of_a_kind, :enter_score)}
+		it {is_expected.to respond_to(:chance, :large_straight, :small_straight, :full_house, :yahtzee, :four_of_a_kind, :three_of_a_kind, :enter_score, :dice)}
 	end
 end
