efaffb8cfbb44910a247a9c99ed20b0ffe91cbea
Zachary Perlmutter <zrp200@gmail.com>
2014-12-08 20:47:41 -0800

    Fixed tests for ScoreSheet
    Signed-off-by: Zachary Perlmutter <zrp200@gmail.com>

--- spec/lib/score_sheet_spec.rb
+++ spec/lib/score_sheet_spec.rb
@@ -6,9 +6,9 @@ describe ScoreSheet do
 		it {is_expected.to respond_to(:chance, :large_straight, :small_straight, :full_house, :yahtzee, :four_of_a_kind, :three_of_a_kind, :enter_score, :dice)}
 		its(:full_house) {is_expected.to eq(25) | be_zero}
 		its(:large_straight) {is_expected.to eq(40) | be_zero}
-		its(:chance) {is_expected be >= 5}
+		its(:chance) {is_expected.to be >= 5}
 		its(:dice) {is_expected.to be_instance_of Dice}
-		its(:sheet) {is_expected.to be_instance of Hash}
+		its(:sheet) {is_expected.to be_instance_of Hash}
 		its(:filled) {is_expected.to be false}
 		its(:small_straight) {is_expected.to eq(30) | be_zero}
 	end
