5c584b8cc23b7fa82567c20fadd4516ad7d4bece
Zachary Perlmutter <zrp200@gmail.com>
2014-12-08 18:52:46 -0800

    Removed unnecessary class

--- lib/dice.rb
+++
@@ -1,8 +0,0 @@
-class Dice # Class for a die in the game
-	attr_reader :num
-	Faces = [1, 2, 3, 4, 5, 6] # Every face on the dice
-	def initialize; @num = Faces.sample; end
-	def roll; @num = Faces.sample; end # Rolls the dice
-	alias to_s num
-end
-
