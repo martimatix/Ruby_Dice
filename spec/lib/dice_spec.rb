3308ae80e422fabb8b2b503b541f50056cfd3fd8
Zachary Perlmutter <zrp200@gmail.com>
2014-12-08 19:11:06 -0800

    Update and rename fivedice_spec.rb to dice_spec.rb

---
+++ spec/lib/dice_spec.rb
@@ -0,0 +1,12 @@
+require "spec_helper"
+require "dice"
+RSpec.describe FiveDice do
+ 	describe "#new" do
+  		describe "dice" do
+    			subject {Dice.new.dice}
+    			it {is_expected.to be_an_instance_of Array}
+		end
+	subject {Dice.new}
+	it {is_expected.to be_an_instance_of Dice}
+ 	end
+end
