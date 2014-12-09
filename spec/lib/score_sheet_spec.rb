9207e48bb2004c33385247a222f18ab730960334
Zachary Perlmutter <zrp200@gmail.com>
2014-12-08 20:38:06 -0800

    Added tests

--- spec/lib/score_sheet_spec.rb
+++ spec/lib/score_sheet_spec.rb
@@ -4,5 +4,9 @@ describe ScoreSheet do
 	describe "#new" do
 		subject {ScoreSheet.new}
 		it {is_expected.to respond_to(:chance, :large_straight, :small_straight, :full_house, :yahtzee, :four_of_a_kind, :three_of_a_kind, :enter_score, :dice)}
+		its(:full_house) {is_expected.to eq(25) | be_zero}
+		its(:large_straight) {is_expected.to eq(40) | be_zero}
+		its(:chance) {is_expected.to_not be_zero}
+		its(:dice) {is_expected.to be_instance_of Dice}
 	end
 end
