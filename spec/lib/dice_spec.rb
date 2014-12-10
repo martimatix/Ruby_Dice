7f4f44bb37576713096eab5dd53ddbd411839517
Zachary Perlmutter <zrp200@gmail.com>
2014-12-09 18:00:56 -0800

    Update dice_spec.rb

--- spec/lib/dice_spec.rb
+++ spec/lib/dice_spec.rb
@@ -2,11 +2,11 @@ require "spec_helper"
 require "dice"
 RSpec.describe Dice do
  	describe "#new" do
+ 		subject {Dice.new}
+ 		it {is_expected.to be_an_instance_of Dice}
   		describe "dice" do
     			subject {Dice.new.dice}
     			it {is_expected.to be_an_instance_of Array}
 		end
-	subject {Dice.new}
-	it {is_expected.to be_an_instance_of Dice}
  	end
 end
