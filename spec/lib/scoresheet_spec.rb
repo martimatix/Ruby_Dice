64202d900d110726354144a3b64d6ff62081ba47
Zachary Perlmutter <zrp200@gmail.com>
2014-12-09 18:23:01 -0800

    Added examples
    Signed-off-by: Zachary Perlmutter <zrp200@gmail.com>

--- spec/lib/scoresheet_spec.rb
+++ spec/lib/scoresheet_spec.rb
@@ -12,7 +12,11 @@ describe ScoreSheet do
 		its(:filled) {is_expected.to be false}
 		its(:yahtzee) {is_expected.to be_zero | eq(50)}
 		its(:small_straight) {is_expected.to eq(30) | be_zero}
+		its(:ones) {is_expected.to satisfy {|value| (value % 1).zero?}}
 		its(:twos) {is_expected.to satisfy {|value| (value % 2).zero?}}
+		its(:threes) {is_expected.to satisfy {|value| (value % 3}.zero?}}
+		its(:fours) {is_expected.to satisfy {|v| (v % 3).zero?}}
+		its(:fives) {is_expected.to satisfy {|v| (v % 3).zero?}}
 	end
 	describe "::UpperScores"
 end
