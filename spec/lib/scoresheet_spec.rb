001937016bfc25d3875b5919ffc7003259614e76
Zachary Perlmutter <zrp200@gmail.com>
2014-12-11 16:35:48 -0800

    Update scoresheet_spec.rb

--- spec/lib/scoresheet_spec.rb
+++ spec/lib/scoresheet_spec.rb
@@ -4,7 +4,6 @@ describe ScoreSheet do
 	describe "#new" do
 		subject {ScoreSheet.new}
 		it {is_expected.to respond_to(:chance, :large_straight, :small_straight, :full_house, :yahtzee, :four_of_a_kind, :three_of_a_kind, :enter_score, :dice)}
-		let(:i) { 1 }
 		its(:full_house) {is_expected.to eq(25) | be_zero}
 		its(:large_straight) {is_expected.to eq(40) | be_zero}
 		its(:chance) {is_expected.to be >= 5}
@@ -13,8 +12,9 @@ describe ScoreSheet do
 		its(:filled) {is_expected.to be false}
 		its(:yahtzee) {is_expected.to be_zero | eq(50)}
 		its(:small_straight) {is_expected.to eq(30) | be_zero}
+		i = 1
 		for score in ScoreSheet::UpperScores
-			its(score) {is_expected.to be_instance_of(Fixnum) & satisfy {|v| (v % i).zero?}}
+			its(score) {is_expected.to be_instance_of(Fixnum) & satisfy {|v| (v % d).zero?}}
 			i += 1
 		end
 	end
