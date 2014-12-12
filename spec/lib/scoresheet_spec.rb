da0f6a6f6465c6787b5395f234650a762d53481e
Zachary Perlmutter <zrp200@gmail.com>
2014-12-12 12:07:47 -0800

    Update scoresheet_spec.rb

--- spec/lib/scoresheet_spec.rb
+++ spec/lib/scoresheet_spec.rb
@@ -1,15 +1,14 @@
 require "spec_helper"
-require "scoresheet"
 describe ScoreSheet do
 	describe "#new" do
 		subject {ScoreSheet.new}
-		it {is_expected.to respond_to(:chance, :large_straight, :small_straight, :full_house, :yahtzee, :four_of_a_kind, :three_of_a_kind, :enter_score, :dice)}
+		it {is_expected.to respond_to(:chance, :filled?, :large_straight, :small_straight, :full_house, :yahtzee, :four_of_a_kind, :three_of_a_kind, :enter_score, :dice)}
 		its(:full_house) {is_expected.to eq(25) | be_zero}
 		its(:large_straight) {is_expected.to eq(40) | be_zero}
 		its(:chance) {is_expected.to be >= 5}
 		its(:dice) {is_expected.to be_instance_of Dice}
 		its(:sheet) {is_expected.to be_instance_of Hash}
-		its(:filled) {is_expected.to be false}
+		its(:filled?) {is_expected.to be false}
 		its(:yahtzee) {is_expected.to be_zero | eq(50)}
 		its(:small_straight) {is_expected.to eq(30) | be_zero}
 		i = 1
