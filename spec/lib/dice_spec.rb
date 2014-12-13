a74d1f0b36346a592269276f790f48725ee64f03
martimatix <zenitram.oiram@gmail.com>
2014-12-13 10:42:10 +0800

    Add test for set_dice

--- spec/lib/dice_spec.rb
+++ spec/lib/dice_spec.rb
@@ -9,4 +9,10 @@ RSpec.describe Dice do
     			it {is_expected.to be_an_instance_of Array}
 		end
  	end
+ 	it "sets the values of the dice" do
+ 		test_dice = Dice.new
+ 		test_val = [1,2,3,4,5]
+ 		test_dice.set_dice(test_val)
+ 		expect(test_dice.dice).to eq(test_val)
+ 	end
 end
