50d79ed0440e737a67a23f4a6f22059ab9a187f9
Zachary Perlmutter <zrp200@gmail.com>
2014-12-13 12:25:43 -0800

    Update dice_spec.rb

--- spec/lib/dice_spec.rb
+++ spec/lib/dice_spec.rb
@@ -8,11 +8,14 @@ RSpec.describe Dice do
     			subject {Dice.new.dice}
     			it {is_expected.to be_an_instance_of Array}
 		end
- 	end
- 	it "sets the values of the dice" do
- 		test_dice = Dice.new
- 		test_val = [1,2,3,4,5]
- 		test_dice.set_dice(test_val)
- 		expect(test_dice.dice).to eq(test_val)
- 	end
+		describe "dice=" do
+			subject do
+				test_dice = Dice.new
+				test_dice.dice = [1, 2, 3, 4, 5]
+				test_dice.dice
+			end
+			let(:test_val) {(1..5).to_a}
+			it {is_expected.to eq test_val}
+		end
+	end
 end
