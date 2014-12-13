b6a73a0fc1ba8171fced2262273bc0c7eb65f182
Zachary Perlmutter <zrp200@gmail.com>
2014-12-13 12:04:58 -0800

    Added easier manipulation of dice.dice

--- lib/scoresheet.rb
+++ lib/scoresheet.rb
@@ -6,8 +6,9 @@ class ScoreSheet
 	attr_reader :sheet # @return [Hash] table of two element arrays where the first value is the score and the second is whether the field has been played
 	attr_reader :dice # @return [Dice]
 	
-	def initialize
+	def initialize(custom_dice=false)
 		@sheet, @dice = Hash.new, Dice.new
+		@dice.dice.set_dice(custom_dice) if custom_dice
 		Array.new(UpperScores).concat(LowerScores).each {|s| @sheet[s] = [0, false]}
 	end
 	
