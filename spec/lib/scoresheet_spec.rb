0783df90bea50e7e8c60933a8e31826d71e17a69
Zachary Perlmutter <zrp200@gmail.com>
2014-12-12 20:58:48 -0800

    Reformatted tests
    * Changed `eq 0` -> `be_zero` 
    * Removed ()s
    * Integrated new tests w/ main section of tests

--- spec/lib/scoresheet_spec.rb
+++ spec/lib/scoresheet_spec.rb
@@ -3,7 +3,6 @@ describe ScoreSheet do
 	describe "#new" do
 		subject {ScoreSheet.new}
 		it {is_expected.to respond_to(:chance, :filled?, :large_straight, :small_straight, :full_house, :yahtzee, :four_of_a_kind, :three_of_a_kind, :enter_score, :dice)}
-		its(:full_house) {is_expected.to eq(25) | be_zero}
 		its(:large_straight) {is_expected.to eq(40) | be_zero}
 		its(:chance) {is_expected.to be >= 5}
 		its(:dice) {is_expected.to be_instance_of Dice}
@@ -11,43 +10,66 @@ describe ScoreSheet do
 		its(:filled?) {is_expected.to be false}
 		its(:yahtzee) {is_expected.to be_zero | eq(50)}
 		its(:small_straight) {is_expected.to eq(30) | be_zero}
+		describe "full_house" do
+			fh = proc do |array|
+				s = ScoreSheet.new
+				s.dice.set_dice array
+				s.full_house
+			end
+			context "when @dice.dice == [1, 1, 2, 2, 2]" do
+				subject {fh.call [1,1,2,2,2]}
+				it {is_expected.to eq 25}
+			end
+			context "when @dice.dice == [2, 2, 2, 2, 2]" do
+				subject {fh.call Array.new(5) {2}}
+				it {is_expected.to eq 25}
+			end
+			context "when @dice.dice == [1, 1, 2, 2, 5]" do
+				subject {fh.call [1,1,2,2,5]
+				it {is_expected.to be_zero}
+			end
+		end
+		describe "three_of_a_kind" do
+			tok = proc do |array|
+				s = ScoreSheet.new
+				s.dice.set_dice array
+				s.three_of_a_kind
+			end
+			context "when @dice.dice == [1, 2, 2, 2, 2]" do
+				subject {tok.call [1, 2, 2, 2, 2]}
+				it {is_expected.to_eq 9}
+			end
+			context "when @dice.dice == [1, 2, 2, 2, 5]" do
+				subject {tok.call [1,2,2,2,5]}
+				it {is_expected.to eq 12}
+			end
+			context "when @dice.dice == [1, 2, 2, 3, 4]" do
+				subject {toc.call [1, 2, 2, 3, 4]}
+				it {is_expected.to be_zero}
+			end
+		end
+		describe "four_of_a_kind" do
+			fok = proc do |array|
+				s = ScoreSheet.new
+				s.dice.set_dice = array
+				s.four_of_a_kind
+			end
+			context "when @dice.dice == [1, 2, 2, 2, 2]" do
+				subject {fok.call [1,2,2,2,2]}
+				it {is_expected.to eq 9}
+			end
+			context "when @dice.dice == [1, 1, 2, 2, 2]" do
+				subject {foc.call [1, 1, 2, 2, 2]}
+				it {is_expected.to be_zero}
+			end
+		end
 		i = 1
 		for score in ScoreSheet::UpperScores
 			its(score) {is_expected.to be_instance_of Fixnum}
 			i += 1
 		end
 	end
-	describe "::UpperScores" do
-		subject {ScoreSheet::UpperScores}
-		for field in ScoreSheet::UpperScores
-			it {is_expected.to be_include field}
-		end
-	end
-	ss = ScoreSheet.new
-	it "calculates full_house correctly" do
-		ss.dice.set_dice([1,1,2,2,2])
-		expect(ss.full_house).to eq(25)
-		ss.dice.set_dice([2,2,2,2,2])
-		expect(ss.full_house).to eq(25)
-		ss.dice.set_dice([1,1,2,2,5])
-		expect(ss.full_house).to eq(0)
-	end
-	it "calculates three_of_a_kind correctly" do
-		ss.dice.set_dice([1,2,2,2,2])
-		expect(ss.three_of_a_kind).to eq(9)
-		ss.dice.set_dice([1,5,2,2,2])
-		expect(ss.three_of_a_kind).to eq(12)
-		ss.dice.set_dice([1,2,3,4,2])
-		expect(ss.three_of_a_kind).to eq(0)
-
-	end
-
-	it "caclulates four_of_a_kind correctly" do
-		ss.dice.set_dice([1,2,2,2,2])
-		expect(ss.four_of_a_kind).to eq(9)
-		ss.dice.set_dice([1,1,2,2,2])
-		expect(ss.four_of_a_kind).to eq(0)
-	end
+	
 end
 
 
