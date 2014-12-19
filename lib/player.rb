a78b0e464c6ebc22648d4c100989a06c2e3e8e75
martimatix <zenitram.oiram@gmail.com>
2014-12-19 11:06:12 +0800

    Minor changes to player.rb

--- lib/player.rb
+++ lib/player.rb
@@ -1,17 +1,18 @@
-require scoresheet
+require_relative "scoresheet"
+require_relative "dice"
 
 class Player
 
-	attr_reader: ss
+	attr_reader :ss
 
-	def intialize(scoresheet)
-		@ss = scoresheet
+	def intialize
+		@ss = ScoreSheet.new
 	end
 
 	def take_turn
 		(1..3).each do |i|
 			puts "Here are your dice"
-			puts ss.dice.to_s
+			puts @ss.dice.to_s
 			puts "You have #{3-i} rolls remaining. Select dice to re-roll or select a score field."
 		end
 	end
