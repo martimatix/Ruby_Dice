a4c5b25d1b7b8076413f8a255ea0134b18a41bc5
martimatix <zenitram.oiram@gmail.com>
2014-12-19 10:09:49 +0800

    Add text to take_turn

--- lib/player.rb
+++ lib/player.rb
@@ -9,5 +9,10 @@ class Player
 	end
 
 	def take_turn
+		(1..3).each do |i|
+			puts "Here are your dice"
+			puts ss.dice.to_s
+			puts "You have #{3-i} rolls remaining. Select dice to re-roll or select a score field."
+		end
 	end
 end
