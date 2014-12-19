d9314a8212f24c27a7ae3610b5d25cf3669c0e36
Zachary Perlmutter <zrp200@gmail.com>
2014-12-18 19:21:25 -0800

    Close #31
    * removed `dice.rb` requirement; `scoresheet.rb` requires it already
    * Fixed misspelled `intialize`; it is spelled `initialize`

--- lib/player.rb
+++ lib/player.rb
@@ -1,19 +1,20 @@
-require_relative "scoresheet"
-require_relative "dice"
+require_relative "scoresheet.rb"
 
 class Player
 
-	attr_reader :ss
+	attr_reader :score
 
-	def intialize
-		@ss = ScoreSheet.new
-	end
+	def initialize; @score = ScoreSheet.new; end
 
+=begin
+@todo finish method
+@return [void]
+=end
 	def take_turn
 		(1..3).each do |i|
-			puts "Here are your dice"
-			puts @ss.dice.to_s
+			puts "Your current stats: \n#{@score.dice.to_s}"
 			puts "You have #{3-i} rolls remaining. Select dice to re-roll or select a score field."
+			input = gets.chomp
 		end
 	end
 end
