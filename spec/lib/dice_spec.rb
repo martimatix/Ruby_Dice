d7368f73f1bbf0655f9138f8b47f724ae47267f8
Zachary Perlmutter <zrp200@gmail.com>
2014-12-22 09:01:04 -0800

    Update dice_spec.rb

--- spec/lib/dice_spec.rb
+++ spec/lib/dice_spec.rb
@@ -8,13 +8,19 @@ RSpec.describe Dice do
     			it {is_expected.to be_an_instance_of Array}
 		end
 		describe "values=" do
-			subject do
-				test_dice = Dice.new
-				test_dice.values = [1, 2, 3, 4, 5]
-				test_dice.values
+			context "when legal" do
+				subject do
+					test_dice = Dice.new [1, 2, 3, 4, 5]
+					test_dice.values
+				end
+				let(:test_val) {(1..5).to_a}
+				it {is_expected.to eq test_val}
 			end
-			let(:test_val) {(1..5).to_a}
-			it {is_expected.to eq test_val}
+			context "when illegal" do
+				it "should raise ArgumentError, \"Array must have 5 Integers that are between 1 and 6\"" do
+					expect {Dice.new %w(1 2 3 4 5 6) }.to raise_error(ArgumentError, "Array must have 5 Integers that are between 1 and 6")
+				end
+			end
 		end
 	end
 end
