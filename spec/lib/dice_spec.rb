657e50d0684d18166a56291c16b922f8ac5cf5af
Zachary Perlmutter <zrp200@gmail.com>
2014-12-26 22:02:05 -0800

    Update dice_spec.rb

--- spec/lib/dice_spec.rb
+++ spec/lib/dice_spec.rb
@@ -9,7 +9,10 @@ RSpec.describe Dice do
     			subject {Dice.new.values}
     			it {is_expected.to be_an_instance_of Array}
 			its(:length) {is_expected.to eq 5}
-			its(:sample) {is_expected.to eq(1 | 2 | 3 | 4 | 5 | 6)}
+			describe "sample" do
+				subject {1..6}
+				it {is_expected.to cover Dice.new.values.sample}
+			end
 		end
 		describe "values=" do
 			context "when [1, 2, 3, 4, 5]" do
