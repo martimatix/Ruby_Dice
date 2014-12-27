4701597a326132377dbe9674cbbd8f7ea6ec10b0
martimatix <zenitram.oiram@gmail.com>
2014-12-27 10:38:08 +0800

    Refactor user_input method

--- lib/player.rb
+++ lib/player.rb
@@ -46,45 +46,65 @@ class Player
 		puts "\tValues\t\t" + score.dice.values.map{|value| value.to_s}.join(?\t)
 		puts String.new.center(80, ?-)
 	end
-	
+
 =begin
 @param i [Fixnum] Amount of times rolled
 @return [Boolean]
 @note Gameplay
 =end
 	def user_input(i)
+
+		print_instructions i
+
+		input = gets.chomp.downcase
+		user_input = Set.new(input.split(''))
+		dice_controls = Set.new("zxcvb".split(''))
+
+		# If user wants to enter score
+		if ScoreAbbr.keys.include? input.to_sym
+			user_enter_score input.to_sym
+		# Else if user wants to roll the dice
+		elsif i < 3 && (user_input.subset? dice_controls)
+			user_roll_dice input, dice_controls
+		else
+			invalid_input
+		end
+	end
+
+	# Private methods need to be indented properly
+	private
+	def print_instructions i
 		if i < 3
 			print "Select dice to re-roll or select a score category: "
 		else
 			print "No rolls remaining. Select a score category: "
 		end
-		input = gets.chomp.downcase
-		input_symbol = input.to_sym
-		user_input = Set.new(input.split(''))
-		dice_controls = Set.new("zxcvb".split(''))
-
-		# If user wants to enter score
-		if ScoreAbbr.keys.include? input_symbol
-			score.enter_score ScoreAbbr[input_symbol]
-			puts score
-			@score.dice.roll_all
-			sleep 2
-			return true
-		# Else if user wants to roll the dice
-		elsif i < 3 && (user_input.subset? dice_controls)
-			dice_to_roll = (0..4).to_a.select { |index| input.include? dice_controls.to_a[index]}
-			@score.dice.roll(dice_to_roll)
-			sleep 0.5
-			2.times {puts ?\n}
-			puts " Rolling Dice!\ ".center 80, "* "
-			sleep 1
-			return false 
-		else
-			puts "Invalid input. Please try again."
-			sleep 1.5
-			puts ?\n
-			user_input i
-		end
 	end
+
+	def user_enter_score input_symbol
+		score.enter_score ScoreAbbr[input_symbol]
+		puts score
+		@score.dice.roll_all
+		sleep 2
+		return true
+	end
+
+	def user_roll_dice input, dice_controls
+		dice_to_roll = (0..4).to_a.select { |index| input.include? dice_controls.to_a[index]}
+		@score.dice.roll(dice_to_roll)
+		sleep 0.5
+		2.times {puts ?\n}
+		puts " Rolling Dice!\ ".center 80, "* "
+		sleep 1
+		return false 
+	end
+
+	def invalid_input
+		puts "Invalid input. Please try again."
+		sleep 1.5
+		puts ?\n
+		user_input i
+	end
+
 end
  
