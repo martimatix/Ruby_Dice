bdc8c3bbb9506b08a045563add37679f525d8451
Zachary Perlmutter <zrp200@gmail.com>
2014-12-27 13:46:11 -0800

    Update player_spec.rb

--- spec/lib/player_spec.rb
+++ spec/lib/player_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe Player do
   describe "#new" do
     subject {Player.new}
     it {is_expected.to respond_to(:take_turn, :display_dice, :user_input)}
-    its(:score) {is_expected.to be_instance_of Scoresheet}
+    its(:score) {is_expected.to be_instance_of ScoreSheet}
   end
 end
